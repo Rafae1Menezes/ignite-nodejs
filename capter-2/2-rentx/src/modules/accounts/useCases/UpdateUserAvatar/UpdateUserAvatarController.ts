48316438b804b0165c69656979eaafa9761a598f
Rafael Menezes <menezes.inbox@gmail.com>
2022-03-07 21:18:45 -0300

    avatar

---
+++ capter-2/2-rentx/src/modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase';
+
+class UpdateUserAvatarController {
+  async handle(request: Request, respose: Response): Promise<Response> {
+    const { id } = request.user;
+    const avatar_file = request.file.filename;
+
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+
+    await updateUserAvatarUseCase.execute({ avatar_file, user_id: id });
+
+    return respose.status(204).send();
+  }
+}
+
+export { UpdateUserAvatarController };
